fff17e7b7c17f3d3113642568130f424f6028f60
Harsh Karn <harshkarn29@gmail.com>
2020-06-25 22:49:15 +0530

    fix/dynamic port support

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 2900;
+const port = process.env.port || 2900;
 
 app.use(express.static(path.join(__dirname + '/static')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/static/index.html')));
